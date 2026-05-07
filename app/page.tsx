1e3e81ca3416797ebae85c58e41dc102c3d852fd
huylc12343 <huylc12343@gmail.com>
2026-05-07 09:52:04 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (<>
-  <Link href="/merch">To merch</Link>
+  <Link href="/merch"><Button>To merch</Button></Link>
   </>)
 };
