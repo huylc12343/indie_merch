cab8f83965ac0aa7fe5b1b8d1658c99df0179089
huylc12343 <huylc12343@gmail.com>
2026-05-08 18:42:25 +0700

    uodate

--- lib/fetchMerch.ts
+++ lib/fetchMerch.ts
@@ -4,7 +4,7 @@ import {MerchItem }from "../lib/types";
 export async function fetchMerch(): Promise<MerchItem[]> {
   const data = await directus.request(
     readItems("merch" as any, {
-      fields: ["*", "merch_images.directus_files_id.*"] as any,
+      fields: ["*", "merch_images.directus_files_id.id"] as any,
     }),
   );
 
