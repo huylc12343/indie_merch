66840358299e86e894458966ad9594a6e38dcda1
huylc12343 <huylc12343@gmail.com>
2026-05-08 19:03:00 +0700

    update

--- lib/fetchMerch.ts
+++ lib/fetchMerch.ts
@@ -2,11 +2,10 @@ import { readItems } from "@directus/sdk";
 import { directus } from "./directus";
 import { MerchItem } from "../lib/types";
 export async function fetchMerch(): Promise<MerchItem[]> {
-  const data = await directus.request(
-    readItems("merch" as any, {
-      fields: ["*", "merch_images.directus_files_id.*"] as any,
-    }),
-  );
-
-  return data as unknown as MerchItem[];
+  const url = `${process.env.NEXT_PUBLIC_DIRECTUS_URL}/items/merch?fields=*,merch_images.directus_files_id.id`;
+  
+  const res = await fetch(url);
+  const json = await res.json();
+  
+  return json.data as MerchItem[];
 }
