acccf546dfdb4bb62228d42e0fc0a152cadb76b8
huylc12343 <huylc12343@gmail.com>
2026-05-08 17:50:17 +0700

    update

--- lib/fetchMerch.ts
+++ lib/fetchMerch.ts
@@ -5,10 +5,22 @@ export async function fetchMerch() {
   const data = await directus.request(
     readItems("merch", {
       fields: [
-        "*"
+        "id",
+        "name",
+        "price",
+        "quantity",
+        "description",
+        "colors",
+        "sizes",
+        "types",
+        "status",
+        {
+          "merchimages": ["directus_files_id"],
+        },
       ],
-    })
+    }),
   );
+
   console.log("Fetched merch data:", data);
   return data;
 }
